9bee8b417d60ea283115a7ae1373dedf0c29e852
Darren Eam <darreneam65@gmail.com>
2025-09-11 09:26:08 -0700

    remove dist output files

--- backend/services/job-initiation-api/dist/middleware/errorHandler.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-export interface AppError extends Error {
-    statusCode?: number;
-    isOperational?: boolean;
-}
-export declare const errorHandler: (error: AppError, req: Request, res: Response) => void;
-export declare const createError: (message: string, statusCode?: number) => AppError;
-//# sourceMappingURL=errorHandler.d.ts.map
--- backend/services/job-initiation-api/dist/routes/jobs.d.ts
+++
@@ -1,3 +0,0 @@
-declare const router: import("express-serve-static-core").Router;
-export { router as jobRoutes };
-//# sourceMappingURL=jobs.d.ts.map
